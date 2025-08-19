a2905e0fac6a5af91b41618315659fafba1770d7
saf-mo <safiamohamud38@gmail.com>
2025-08-19 13:47:02 +0100

    second iteration

--- game.js
+++ game.js
@@ -13,6 +13,30 @@ print() {
     document.getElementById("game-board").innerHTML =
     this.field.map(row => row.join(" ")).join("<br>");
 }
+
+move(direction) {
+  if (direction === "up") this.playerRow--;
+  if (direction === "down") this.playerRow++;
+  if (direction === "left") this.playerCol--;
+  if (direction === "right") this.playerCol++;
+}
+
+checkStatus() {
+    if (
+    this.playerRow < 0 ||
+    this.playerRow >= this.field.length ||
+    this.playerCol < 0 ||
+    this.playerCol >= this.field[0].length
+  ) {
+    return "out";
+  } else if (this.field[this.playerRow][this.playerCol] === hole) {
+    return "hole";
+  } else if (this.field[this.playerRow][this.playerCol] === hat) {
+    return "hat";
+  } else {
+    return "safe";
+  }};
+
 static generateField() {
     let rows = 3;
     let columns = 3;
@@ -20,7 +44,7 @@ static generateField() {
     for (let i = 0; i < rows; i++) {
       let row = [];
       for (let j = 0; j < columns; j++) {
-        row.push("░");
+        row.push(fieldCharacter);
       }
       field.push(row);
     }
@@ -31,9 +55,9 @@ static generateField() {
       let randomCol = Math.floor(Math.random() * 3);
       if (
         (randomRow !== 0 || randomCol !== 0) &&
-        field[randomRow][randomCol] !== "O"
+        field[randomRow][randomCol] !== hole
       ) {
-        field[randomRow][randomCol] = "O";
+        field[randomRow][randomCol] = hole;
         numHoles--;
       }
     }
@@ -43,13 +67,13 @@ static generateField() {
       let randomCol = Math.floor(Math.random() * columns);
       if (
         (randomRow !== 0 || randomCol !== 0) && // not the start
-        field[randomRow][randomCol] !== "O" // not a hole
+        field[randomRow][randomCol] !==  hole // not a hole
       ) {
-        field[randomRow][randomCol] = "^";
+        field[randomRow][randomCol] = hat;
         hatPlaced = true;
       }
     }
-    field[0][0] = "*";
+    field[0][0] = pathCharacter;
     return field;
   }
 
@@ -59,20 +83,22 @@ const myField = new Field(Field.generateField());
 myField.print();
 
 document.addEventListener("keydown", (e) => {
+
     if (e.key === "ArrowUp") myField.move("up");
     if (e.key === "ArrowDown") myField.move("down");
     if (e.key === "ArrowLeft") myField.move("left");
     if (e.key === "ArrowRight") myField.move("right");
 
+
     const status = myField.checkStatus();
     if (status === "safe") {
-        myField.field[myField.playerRow][myField.playerCol] = "*"
-    }
-    myField.print();
+  myField.field[myField.playerRow][myField.playerCol] = pathCharacter;
+}
+myField.print();
 
-    if (status === "out" || status === "hole" || status === "hat"){
-    alert(status === "hat" ? "YOU FOUND THE HAT!" : "Game Over!");
-    location.reload();
+if (status === "out" || status === "hole" || status === "hat") {
+  alert(status === "hat" ? "YOU FOUND THE HAT!" : "Game Over!");
+  location.reload();
 }
 });
 
